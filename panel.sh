98e65ddd75a610f16abdb0be4583ffbaad540bd7
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2023-12-25 13:17:46 -0500

    edit: panel.sh

--- panel.sh
+++ panel.sh
@@ -1,2 +1,35 @@
- #!/bin/bash
+#!/bin/bash
 
+##
+#
+# Data Init
+#
+#######
+
+# --- List of GLOBAL variables --- #
+GLOBAL_PWD=$(pwd)
+GLOBAL_NEED_RESTART=0 #NEED TO RESTART AFTER ANY FAIL ARGUMENT INIT (OR ALL ARGUMENT INIT AND BEFORE ANOTHER ARGUMENT CHANGE)
+GLOBAL_MODULES=()
+GLOBAL_ENVIRONMENTS=()
+GLOBAL_CURRENT_MODULE=""
+GLOBAL_CURRENT_ENVIRONMENT=""
+
+# --- Base Libraries Init --- #
+source ./core/.index.sh
+
+# --- GET SCRIPT ARGUMENTS --- #
+while getopts ":m:e:" flag
+do
+    case "${flag}" in
+        m) ARG_MODULE=${OPTARG};; # module
+        e) ARG_ENVIRONMENT=${OPTARG};; # environment
+    esac
+done
+
+##
+#
+# Run Panel
+#
+#######
+
+panel-initialization $ARG_MODULE $ARG_ENVIRONMENT
