bc65c50ecdb5b69edd353b387759642f8bfb8b9f
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2023-12-26 11:49:59 -0500

    edit: default argument value

--- core/initialization.sh
+++ core/initialization.sh
@@ -11,8 +11,8 @@ function panel-initialization {
     # TODO: check if all SCRIPT COMMANDS works -> throw-handy-error
     # TODO: check if BASE BUNDLE is installed -> try to install base_bundle.sh
     
-    local LOCAL_MODULE=${1:-"!"}
-    local LOCAL_ENVIRONMENT=${2:-"!"}
+    local LOCAL_MODULE=${1:-"NONE"}
+    local LOCAL_ENVIRONMENT=${2:-"NONE"}
     
     change-module $LOCAL_MODULE $LOCAL_ENVIRONMENT
     
@@ -24,14 +24,14 @@ function change-module {
     # TODO: modules must be pattern *.wisent
     # TODO: check basic structure/integrity of modules & environment
     
-    local LOCAL_MODULE=${1:-"!"}
-    local LOCAL_ENVIRONMENT=${2:-"!"}
+    local LOCAL_MODULE=${1:-"NONE"}
+    local LOCAL_ENVIRONMENT=${2:-"NONE"}
     
     folder-argument-logic GLOBAL_CURRENT_MODULE $LOCAL_MODULE GLOBAL_MODULES "MODULE" "./module" "^[a-z][a-z0-9_.-]*$"
     
     if [ "$GLOBAL_CURRENT_MODULE" != "$LOCAL_MODULE" ]
     then
-        LOCAL_ENVIRONMENT="!"
+        LOCAL_ENVIRONMENT="NONE"
     fi
     
     change-environment $LOCAL_ENVIRONMENT
@@ -39,7 +39,7 @@ function change-module {
 
 function change-environment {
     
-    local LOCAL_ENVIRONMENT=${1:-"!"}
+    local LOCAL_ENVIRONMENT=${1:-"NONE"}
     
     folder-argument-logic GLOBAL_CURRENT_ENVIRONMENT $LOCAL_ENVIRONMENT GLOBAL_ENVIRONMENTS  "ENVIRONMENT" "./module/${GLOBAL_CURRENT_MODULE}/env" "^[a-z][a-z0-9_.-]*$"
     
