12642655cb7154a41e3c2309a4235e5438973083
Vladyslav Kunitskyi <the.kunitskyi@gmail.com>
2023-12-26 11:57:43 -0500

    fix: reference issue in argument

--- core/general/_argument.sh
+++ core/general/_argument.sh
@@ -11,19 +11,21 @@
 
 function folder-argument-logic {
     
-    local -n CURRENT_ARGUMENT="$1"
+    local CURRENT_ARGUMENT_NAME="$1"
     local ARGUMENT_VALUE="$2"
-    local -n REFERENCE_GLOBAL_ARGUMENTS="$3"
+    local ARGUMENTS_NAME="$3"
     local ARGUMENT_NAME="$4"
     local ARGUMENT_DIR="$5"
     local ARGUMENT_PATTERN="$6"
+    local -n CURRENT_ARGUMENT_REFERENCE="${CURRENT_ARGUMENT_NAME}"
+    local -n ARGUMENTS_REFERENCE="${ARGUMENTS_NAME}"
     local USER_INPUT=""
     
-    generate-folder-argument-array REFERENCE_GLOBAL_ARGUMENTS $ARGUMENT_DIR $ARGUMENT_PATTERN
+    generate-folder-argument-array $ARGUMENTS_NAME $ARGUMENT_DIR $ARGUMENT_PATTERN
     
-    if check-value-in-array "${ARGUMENT_VALUE}" "${REFERENCE_GLOBAL_ARGUMENTS[@]}"
+    if check-value-in-array "${ARGUMENT_VALUE}" "${ARGUMENTS_REFERENCE[@]}"
     then
-        CURRENT_ARGUMENT="${ARGUMENT_VALUE}"
+        CURRENT_ARGUMENT_REFERENCE="${ARGUMENT_VALUE}"
     else
         
         GLOBAL_NEED_RESTART=1 # !!! BE AWARE GLOBAL VARIABLE HERE
@@ -31,18 +33,18 @@ function folder-argument-logic {
         show-header
         echo -e "\033[1;37mSelect \033[33m${ARGUMENT_NAME}\033[37m argument:\033[0m \033[1;2;3;32m(Enter FULL name of argument)\033[0m"
         
-        for ITEM in "${REFERENCE_GLOBAL_ARGUMENTS[@]}"
+        for ITEM in "${ARGUMENTS_REFERENCE[@]}"
         do
             echo " - ${ITEM}"
         done
         
         custom-read USER_INPUT
         
-        if check-value-in-array "$USER_INPUT" "${REFERENCE_GLOBAL_ARGUMENTS[@]}"
+        if check-value-in-array "$USER_INPUT" "${ARGUMENTS_REFERENCE[@]}"
         then
-            CURRENT_ARGUMENT="${USER_INPUT}"
+            CURRENT_ARGUMENT_REFERENCE="${USER_INPUT}"
         else
-            folder-argument-logic $CURRENT_ARGUMENT $ARGUMENT_VALUE $REFERENCE_GLOBAL_ARGUMENTS $ARGUMENT_NAME $ARGUMENT_DIR $ARGUMENT_PATTERN
+            folder-argument-logic $CURRENT_ARGUMENT_NAME $ARGUMENT_VALUE $ARGUMENTS_NAME $ARGUMENT_NAME $ARGUMENT_DIR $ARGUMENT_PATTERN
         fi
     fi
     
@@ -64,7 +66,7 @@ function generate-folder-argument-array {
     do
         FOLDER_NAME=$(basename "${ITEM}")
         
-        if [[ "${FOLDER_NAME}" =~ $ARGUMENT_PATTERN ]]
+        if [[ "${FOLDER_NAME}" =~ $ARGUMENT_PATTERN && "${FOLDER_NAME}" != "NONE" ]]
         then
             REFERENCE_OF_GLOBAL_ARGUMENTS+=("${FOLDER_NAME}")
         fi
